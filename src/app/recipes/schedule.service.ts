48f4af0f670bd57485c21600f1fbeb2e2e7f9f0e
alexander.bondarev <alex@strangeloopgames.com>
2020-12-25 16:22:18 +0300

    1. Refactored suggestions to auto-apply suffix. 2. Fixed output for 22, 23, 24 etc.

--- src/app/recipes/schedule.service.ts
+++ src/app/recipes/schedule.service.ts
@@ -5,56 +5,66 @@ import {toNumber, map as _map, filter as _filter, isArray} from 'lodash-es';
   providedIn: 'root'
 })
 export class ScheduleService {
-  static regex = /^(?<dosageCount>\d+)(?: (?<dosageUnit>таблет(?:ка|ки|ок)|капсул[а|ы]?)(?: ((?<daily>каждый день)|каждые (?<intervalDays>(?:[2-9]|[1-9]\d+))(?<intervalDaysSuffix> дн(?:я|ей))?)(?: (до|после|за \d+ минуты? до|через \d+ минуты? после)(?: (завтрака|полдника|обеда|ужина))?)?)?)?/;
+  static regex = /^(?<dosageCount>\d+)(?: (?<dosageUnit>таблет(?:ка|ки|ок)|капсул[а|ы]?)(?: ((?<daily>каждый день)|каждые (?<intervalDays>(?:[1-9]\d+|[2-9]))(?<intervalDaysSuffix> дн(?:я|ей))?)(?: (до|после|во время|за \d+ минуты? до|через \d+ минуты? после)(?: (завтрака|полдника|обеда|ужина))?)?)?)?/;
 
   constructor() { }
+
+  private static isFew(count: number): boolean {
+    if (count >= 5 && count <= 20) { return false; }
+    count %= 10;
+    return count > 0 && count < 5;
+  }
 
   private static generateDosageSuggestions(count: number|undefined): (string|[string, string])[] {
     if (count !== undefined && count <= 0) {
       count = undefined;
     }
     if (count === undefined) {
-      return [['X таблеток', '']];
+      return [['X таблеток', ''], ['X капсул', '']];
     } else if (count === 1) {
-      return ['1 таблетка', '1 капсула'];
-    } else if (count < 5) {
-      return [count + ' таблетки', count + ' капсулы'];
+      return [' таблетка', ' капсула'];
+    } else if (this.isFew(count)) {
+      return [' таблетки', ' капсулы'];
     } else {
-      return [count + ' таблеток', count + ' капсул'];
+      return [' таблеток', ' капсул'];
     }
   }
 
-  private static generateIntervalSuggestions(prefix: string, count: number|undefined): (string|[string, string])[] {
+  private static generateIntervalSuggestions(count: number|undefined): (string|[string, string])[] {
     if (count === undefined) {
       return [
-        prefix + ' каждый день', prefix + ' каждые 2 дня', prefix + ' каждые 5 дней',
-        [prefix + ' каждые X дней', prefix + ' каждые ']
+        ' каждый день', ' каждые 2 дня', ' каждые 5 дней',
+        [' каждые X дней', ' каждые ']
       ];
-    } else if (count < 5) {
-      return [prefix + ' дня'];
+    } else if (this.isFew(count)) {
+      return [' дня'];
     } else {
-      return [prefix + ' дней'];
+      return [' дней'];
     }
+  }
+
+  private static generateEventSuggestions(offset: number|undefined): (string|[string, string])[] {
+    if (offset === undefined) {
+      return [' до', ' после', 'во время', [' за X минут до', ' за'], [' через X минут после', ' через ']];
+    }
+    return [];
   }
 
   generateSuggestions(text: string): [string, string][] {
-    console.log(text);
-    const suggestions = _map(this.generateSuggestionsInternal(text),
-        x => (isArray(x) ? x as [string, string] : [x, x]) as [string, string]);
+    const match = ScheduleService.regex.exec(text);
+    const prefix = match && match[0] || '';
+    const suggestions = _map(this.generateSuggestionsInternal(match),
+        x => (isArray(x) ? [prefix + x[0], prefix + x[1]] as [string, string] : [prefix + x, prefix + x]) as [string, string]);
     const filteredSuggestions = _filter(suggestions, x => {
-      console.log('filter', x, x[0], text, x[0].startsWith(text));
       return x[0].startsWith(text);
     });
-    console.log(suggestions, filteredSuggestions);
     return filteredSuggestions.length > 0 ? filteredSuggestions : suggestions;
   }
 
-  generateSuggestionsInternal(text: string): (string|[string, string])[] {
-    const match = ScheduleService.regex.exec(text);
-    console.log(match);
+  generateSuggestionsInternal(match: RegExpExecArray|null): (string|[string, string])[] {
     if (match == null || match.groups === undefined)
     {
-      return ScheduleService.generateDosageSuggestions(1);
+      return ScheduleService.generateDosageSuggestions(undefined);
     }
     else if (match.groups.dosageUnit === undefined)
     {
@@ -63,9 +73,9 @@ export class ScheduleService {
     else if (match.groups.daily === undefined && match.groups.intervalDaysSuffix === undefined)
     {
       const count = match.groups.intervalDays !== undefined ? toNumber(match.groups.intervalDays) : undefined;
-      return ScheduleService.generateIntervalSuggestions(match[0], count);
+      return ScheduleService.generateIntervalSuggestions(count);
     }
 
-    return ScheduleService.generateDosageSuggestions(1);
+    return [];
   }
 }
