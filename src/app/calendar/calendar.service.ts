5eddf95f45b9763dbab455e64803ebae64deb6a5
KGOH <kgofhedgehogs@gmail.com>
2020-12-25 23:14:56 +0200

    Uncomment formatted calendar

--- src/app/calendar/calendar.service.ts
+++ src/app/calendar/calendar.service.ts
@@ -16,19 +16,6 @@ export class CalendarService {
   constructor(private recipeService: RecipeService) { }
 
   public getCalendarEvents(date: Date): CalendarEvent[] {
-    // return shopping_list.core.formatted_schedule_for_a_day(date, this.recipeService.recipes);
-    return [{
-      event: `За 30 минут до завтрака`,
-      drugs: [
-        'Стокрин 600 мг таблетка, 2 шт',
-        'Стн 600 мг таблетка, 2 шт'
-      ]
-    }, {
-      event: `До обеда`,
-      drugs: [
-        'Стокрин 200 мг таблетка, 2 шт',
-        'Стн 300 мг таблетка, 2 шт'
-      ]
-    }] as CalendarEvent[];
+    return shopping_list.core.formatted_schedule_for_a_day(date, this.recipeService.recipes);
   }
 }
