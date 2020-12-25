4844a5cc3c70a2c2f192cede30c3dde494398f15
alexander.bondarev <alex@strangeloopgames.com>
2020-12-25 10:47:50 +0300

    1. Add @angular/material and lodash. 2. Added basic form for schedule with auto-complete.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { AppComponent } from './app.component';
 import { RecipeViewComponent } from './recipes/recipe-view/recipe-view.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { ScheduleComponent } from './recipes/schedule/schedule.component';
-import {MatFormField, MatFormFieldModule} from '@angular/material/form-field';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -24,8 +26,11 @@ import {MatFormField, MatFormFieldModule} from '@angular/material/form-field';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    BrowserAnimationsModule,
     MatAutocompleteModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/recipes/schedule/schedule.component.ts
+++ src/app/recipes/schedule/schedule.component.ts
@@ -2,6 +2,9 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Schedule} from '../../domain/Schedule';
 import {Drug} from '../../domain/Drug';
 import {from, Observable} from 'rxjs';
+import {FormControl, FormGroup} from '@angular/forms';
+import {map, startWith} from 'rxjs/operators';
+import {filter} from 'lodash-es';
 
 @Component({
   selector: 'app-schedule',
@@ -10,11 +13,23 @@ import {from, Observable} from 'rxjs';
 })
 export class ScheduleComponent implements OnInit {
   @Input() public schedule!: Schedule;
-  public filteredDrugs: Observable<Drug[]> = from([]);
+  private drugControl = new FormControl();
+  public form = new FormGroup({
+    drug: this.drugControl
+  });
+  public filteredDrugs: Observable<Drug[]> = from([[]]);
   constructor() { }
 
   ngOnInit(): void {
-    this.filteredDrugs = from([[new Drug('Аспирин'), new Drug('Активированный уголь')]]);
+    this.filteredDrugs = this.drugControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
   }
-
+  public drugName(drug: Drug): string {
+    return drug && drug.name;
+  }
+  private _filter(value: string): Drug[] {
+    return filter([new Drug('Аспирин'), new Drug('Активированный уголь')], x => x.name.startsWith(value));
+  }
 }
